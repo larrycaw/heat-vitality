ecff1d29f2a3b2c0b18399510b7966dfc32ccad4
Isak Haknes <isakhauknes@gmail.com>
2021-05-05 23:00:24 +0200

    edited old golve_control name

---
+++ ios/Flutter/Flutter.h
@@ -0,0 +1,18 @@
+//
+//  Flutter.h
+//  Flutter
+//
+//  Created by Isak Hauknes on 05/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Flutter.
+FOUNDATION_EXPORT double FlutterVersionNumber;
+
+//! Project version string for Flutter.
+FOUNDATION_EXPORT const unsigned char FlutterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flutter/PublicHeader.h>
+
+
