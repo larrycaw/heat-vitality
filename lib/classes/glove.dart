6f793164fc2d1d78b4d7481ba3fd5224233b06d0
add_me <jonas.mela@gmail.com>
2021-05-10 22:33:38 +0200

    Added attributes to glove class

--- lib/classes/glove.dart
+++ lib/classes/glove.dart
@@ -4,49 +4,71 @@ class Glove {
   int battery;
   int heatStep;
   int heatCustom;
+  bool myDevice;
+  bool isConnecting;
+  bool isConnected;
 
-  Glove({this.name, this.desc, this.battery, this.heatStep, this.heatCustom});
+  Glove({this.name, this.desc, this.battery, this.heatStep, this.heatCustom, this.myDevice, this.isConnecting, this.isConnected});
 
   static List<Glove> getGloves() {
     return <Glove>[
       Glove(
-          name: "Isak",
-          desc: "Varmehanske fra At Ease",
-          battery: 100,
-          heatStep: 0,
-          heatCustom: 0),
+        name: "Isak",
+        desc: "Varmehanske fra At Ease",
+        battery: 100,
+        heatStep: 0,
+        heatCustom: 0,
+        myDevice: true,
+        isConnecting: false,
+        isConnected: false,
+      ),
       Glove(
-          name: "Adrian",
-          desc: "Varmehanske fra At Ease",
-          battery: 40,
-          heatStep: 0,
-          heatCustom: 0),
+        name: "Adrian",
+        desc: "Varmehanske fra At Ease",
+        battery: 40,
+        heatStep: 0,
+        heatCustom: 0,
+        myDevice: false,
+        isConnecting: false,
+        isConnected: false,
+      ),
       Glove(
-          name: "Markus Solheim",
-          desc: "nigger",
-          battery: 69,
-          heatStep: 0,
-          heatCustom: 0)
+        name: "Markus Solheim",
+        desc: "nigger",
+        battery: 69,
+        heatStep: 0,
+        heatCustom: 0,
+        myDevice: false,
+        isConnecting: false,
+        isConnected: false,
+      )
     ];
   }
 
   get getDesc => this.desc;
-
   set setDesc(String desc) => this.desc = desc;
 
   get getName => name;
-
   set setName(String name) => this.name = name;
 
   get getBattery => battery;
-
   set setBattery(battery) => this.battery = battery;
 
   get getHeatStep => this.heatStep;
-
   set setHeatStep(int heatStep) => this.heatStep = heatStep;
 
   get getHeatCustom => this.heatCustom;
-
   set setHeatCustom(int heatCustom) => this.heatCustom = heatCustom;
+
+  get getMyDevice => this.myDevice;
+  void addDeviceToMyDevices() => this.myDevice = true;
+  void removeDeviceFromMyDevices() => this.myDevice = false;
+
+  get getIsConnecting => this.isConnecting;
+  void startConnecting() => this.isConnecting = true;
+  void stopConnecting() => this.isConnecting = false;
+
+  get getIsConnected => this.isConnected;
+  void connect() => this.isConnected = true;
+  void disconnect() => this.isConnected = false;
 }
