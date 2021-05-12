ad314518c9e383deaed429ac3b8c86efdfaca3fc
Isak Hauknes <isakhauknes@gmail.com>
2021-05-12 19:52:14 +0200

    dashboard now shows a textstring when no devices connected

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -12,53 +12,68 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  bool isConnected() {
+    // See if any bt devices are registered on MyDevice
+    for (Glove glove in gloves) {
+      if (glove.isConnected) return true;
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-          onWillPop: () async => false,
-          child: Scaffold(
+      onWillPop: () async => false,
+      child: Scaffold(
         // Tilkoblede produkter
         // (Fremtidig forbedring: hente enheter fra liste generert fra tilkoblede enheter)
         body: Container(
           margin: const EdgeInsets.all(20.0),
-          child: ListView(
-            children: [
-              Text(
-                'units'.tr(),
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                //textAlign: TextAlign.left,
-              ),
-              for (Glove glove in gloves)
-                if (glove.getIsConnected)
-                  GestureDetector(
-                    onTap: () => Navigator.pushNamed(
-                      context,
-                      "/glove_settings",
-                      arguments: <String, Glove>{
-                        'glove': glove,
-                      },
-                    ).then((value) => setState(() {})),
-                    child: Card(
-                      elevation: 15,
-                      child: Column(children: <Widget>[
-                        ListTile(
-                            leading: Container(
-                                child: Icon(
-                                  GloveIcon.glove,
-                                  size: 30.0,
-                                )),
-                            title:
-                            Text(glove.getName + "'s " + "heatGlove".tr()),
-                            subtitle: Text(glove.getBattery.toString() +
-                                "% " +
-                                "battery".tr())),
-                      ],
-                      ),
+          child: isConnected()
+              ? ListView(
+                  children: [
+                    Text(
+                      'units'.tr(),
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      //textAlign: TextAlign.left,
                     ),
-                  ),
-            ],
-          ),
+                    for (Glove glove in gloves)
+                      if (glove.getIsConnected)
+                        GestureDetector(
+                          onTap: () => Navigator.pushNamed(
+                            context,
+                            "/glove_settings",
+                            arguments: <String, Glove>{
+                              'glove': glove,
+                            },
+                          ).then((value) => setState(() {})),
+                          child: Card(
+                            elevation: 15,
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                    leading: Container(
+                                        child: Icon(
+                                      GloveIcon.glove,
+                                      size: 30.0,
+                                    )),
+                                    title: Text(glove.getName +
+                                        "'s " +
+                                        "heatGlove".tr()),
+                                    subtitle: Text(glove.getBattery.toString() +
+                                        "% " +
+                                        "battery".tr())),
+                              ],
+                            ),
+                          ),
+                        ),
+                  ],
+                )
+              : Center(
+                  child: Text("connectToStart".tr(),
+                      style: TextStyle(fontSize: 16)),
+                ),
         ),
         // Navigasjonsbar på bunnen av siden
         bottomNavigationBar: BottomAppBar(
