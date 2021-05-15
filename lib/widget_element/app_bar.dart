92f6b810a69bf654d7876788ccbe2c83377284f2
Isak Haknes <isakhauknes@gmail.com>
2021-05-15 17:34:12 +0200

    removed app_bar_back.dart + play store signing

--- lib/widget_element/app_bar.dart
+++ lib/widget_element/app_bar.dart
@@ -18,13 +18,6 @@ class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      /*title: Text(
-        'HeatVitality',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 24,
-        ),
-      ),*/
       centerTitle: true,
       backgroundColor: Color(0xFF6223EE),
       elevation: 0,
